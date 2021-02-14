e96882738dfb47a7f5ef94e79397d72f5b30675f
Ekta18215 <67329015+Ekta18215@users.noreply.github.com>
2021-02-14 21:00:11 +0530

    Update myfile.js

--- myfile.js
+++ myfile.js
@@ -1,11 +1,1 @@
-var nav=document.getElementById('nav');
-window.onscroll=function(){
-	if(window.pageYOffset >100){
-		nav.style.background="#007bff";
-		nav.style.boxshadow="0px 4px 2px blue";
-	}
-	else{
-		nav.style.background="transparent";
-		nav.style.boxshadow="none";
-	}
-}
+
